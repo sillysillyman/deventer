027a3fdad8a21090adb7fde8754fab0bf809c0d5
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-03 02:41:24 +0900

    refactor: UserDetailsServiceImpl로 클래스명 변경

--- src/main/java/io/sillysillyman/deventer/security/UserDetailsServiceImpl.java
+++ src/main/java/io/sillysillyman/deventer/security/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
