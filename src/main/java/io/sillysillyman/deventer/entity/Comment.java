c973cb80359701293b42f208d006f0601e622fbf
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-05 00:39:45 +0900

    feat: Comment 엔티티에 좋아요 기능 추가
    - Comment 엔티티에 CommentLike 리스트 추가
    - Comment와 CommentLike의 양방향 매핑 설정

--- src/main/java/io/sillysillyman/deventer/entity/Comment.java
+++ src/main/java/io/sillysillyman/deventer/entity/Comment.java
@@ -1,5 +1,7 @@
 package io.sillysillyman.deventer.entity;
 
+import io.sillysillyman.deventer.entity.like.CommentLike;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,16 +9,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 @Table(name = "comments")
 public class Comment extends Timestamped {
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<CommentLike> likes = new ArrayList<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
