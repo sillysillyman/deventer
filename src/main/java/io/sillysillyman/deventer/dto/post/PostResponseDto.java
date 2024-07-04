b1ace17a6b6c994af5c2d788b2a76e85bf9f1bb8
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-04 11:30:53 +0900

    refactor: 필드명 수정
    - 생성 및 수정 시간 필드명 수정
      - createAt -> createdAt
      - updateAt -> updatedAt

--- src/main/java/io/sillysillyman/deventer/dto/post/PostResponseDto.java
+++ src/main/java/io/sillysillyman/deventer/dto/post/PostResponseDto.java
@@ -1,8 +1,8 @@
 package io.sillysillyman.deventer.dto.post;
 
 import io.sillysillyman.deventer.entity.Post;
-import lombok.Getter;
 import java.time.LocalDateTime;
+import lombok.Getter;
 
 @Getter
 public class PostResponseDto {
@@ -11,16 +11,15 @@ public class PostResponseDto {
     private final String categoryTopic;
     private final String title;
     private final String content;
-    private final LocalDateTime createAt;
-    private final LocalDateTime updateAt;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
 
     public PostResponseDto(Post post) {
         this.nickname = post.getUser().getNickname();
         this.categoryTopic = post.getCategory().getTopic();
         this.title = post.getTitle();
         this.content = post.getContent();
-        this.createAt = post.getCreatedAt();
-        this.updateAt = post.getUpdateAt();
+        this.createdAt = post.getCreatedAt();
+        this.updatedAt = post.getUpdatedAt();
     }
-
 }
