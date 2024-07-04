e01b97be043acee5564a3022e864d7bff94367b7
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-04 11:18:15 +0900

    refactor: 예외 클래스명 변경
    - 처리하는 예외 경우와 적절히 대응하지 않으므로 클래스명 변경

--- src/main/java/io/sillysillyman/deventer/exception/UserActionNotAllowedException.java
+++ src/main/java/io/sillysillyman/deventer/exception/UserActionNotAllowedException.java
@@ -2,9 +2,9 @@ package io.sillysillyman.deventer.exception;
 
 import io.sillysillyman.deventer.enums.UserActionError;
 
-public class MismatchStatusException extends RuntimeException {
+public class UserActionNotAllowedException extends RuntimeException {
 
-    public MismatchStatusException(UserActionError userActionError) {
+    public UserActionNotAllowedException(UserActionError userActionError) {
         super(userActionError.getMessage());
     }
 }
