5d15072ea73b9142150c2070e7803d2f76ce9cd8
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-05 01:16:44 +0900

    feat: ProfileResponseDto에 게시물 및 댓글 좋아요 개수 추가
    - ProfileResponseDto에 postLikeCount와 commentLikeCount 필드를 추가하여 사용자가 누른 좋아요 개수 포함
    - User 엔티티로부터 postLikes와 commentLikes의 크기를 가져와서 설정하도록 생성자 수정

--- src/main/java/io/sillysillyman/deventer/dto/user/ProfileResponseDto.java
+++ src/main/java/io/sillysillyman/deventer/dto/user/ProfileResponseDto.java
@@ -9,12 +9,16 @@ public class ProfileResponseDto {
     private final Long userId;
     private final String username;
     private final String nickname;
+    private final long postLikeCount;
+    private final long commentLikeCount;
     private final String email;
 
     public ProfileResponseDto(User user) {
         this.userId = user.getId();
         this.username = user.getUsername();
         this.nickname = user.getNickname();
+        this.postLikeCount = user.getPostLikes().size();
+        this.commentLikeCount = user.getCommentLikes().size();
         this.email = user.getEmail();
     }
 }
