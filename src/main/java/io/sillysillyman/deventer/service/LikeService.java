c19085d7e6cf2347f3940b4b91bb55626396fbb6
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-05 00:32:46 +0900

    refactor: 좋아요 서비스 클래스 리팩토링 및 제네릭화
    - 기존 LikeService 클래스를 제네릭화하여 다양한 엔티티에 대해 좋아요 기능을 확장 가능하도록 수정
    - 엔티티에 대한 좋아요 토글 및 좋아요 목록 조회 기능을 포함
    - 엔티티 소유권 검증 및 락을 통해 동시성 제어

--- src/main/java/io/sillysillyman/deventer/service/LikeService.java
+++ src/main/java/io/sillysillyman/deventer/service/LikeService.java
@@ -1,126 +1,83 @@
 package io.sillysillyman.deventer.service;
 
-import io.sillysillyman.deventer.entity.Comment;
-import io.sillysillyman.deventer.entity.Like;
-import io.sillysillyman.deventer.entity.Post;
 import io.sillysillyman.deventer.entity.User;
-import io.sillysillyman.deventer.enums.LikeableEntityType;
-import io.sillysillyman.deventer.enums.NotFoundEntity;
 import io.sillysillyman.deventer.enums.UserActionError;
-import io.sillysillyman.deventer.exception.EntityNotFoundException;
 import io.sillysillyman.deventer.exception.UserActionNotAllowedException;
-import io.sillysillyman.deventer.repository.CommentRepository;
-import io.sillysillyman.deventer.repository.LikeRepository;
-import io.sillysillyman.deventer.repository.PostRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.PersistenceContext;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
-public class LikeService {
-
-    private final CommentRepository commentRepository;
-    private final PostRepository postRepository;
-    private final LikeRepository likeRepository;
+public abstract class LikeService<T, L, D> {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     /**
-     * 좋아요를 처리합니다. 이미 좋아요가 되어있으면 취소하고, 그렇지 않으면 좋아요를 추가합니다.
+     * 엔티티에 좋아요를 토글합니다.
      *
-     * @param likeableEntityId   좋아요를 할 엔티티의 ID
-     * @param likeableEntityType 좋아요를 할 엔티티의 타입 (POST 또는 COMMENT)
-     * @param user               현재 인증된 사용자 정보
-     * @return 좋아요가 추가되었으면 true, 취소되었으면 false
+     * @param entityId 엔티티 ID
+     * @param user     현재 인증된 사용자 정보
+     * @return 좋아요 처리 결과 메시지
      */
     @Transactional
-    public boolean toggleLike(Long likeableEntityId, String likeableEntityType, User user) {
-        LikeableEntityType entityType = LikeableEntityType.getByType(likeableEntityType);
+    public String toggleLike(Long entityId, User user) {
+        T entity = entityManager.find(getEntityClass(), entityId, LockModeType.PESSIMISTIC_WRITE);
 
-        lockEntity(likeableEntityId, entityType);
+        validateOwnership(getEntityOwnerId(entity), user.getId());
 
-        Optional<Like> optionalLike = likeRepository.findByLikeableEntityIdAndLikeableEntityTypeAndUserId(
-            likeableEntityId, entityType, user.getId());
-
+        Optional<L> optionalLike = findLikeByEntityAndUser(entity, user);
         if (optionalLike.isEmpty()) {
-            validateLikeableEntityOwnership(likeableEntityId, entityType, user);
-            Like like = new Like(likeableEntityId, likeableEntityType, user);
-            likeRepository.save(like);
-            return true;
+            L like = createLike(user, entity);
+            getLikeRepository().save(like);
+            return getEntityName() + "(Id: " + entityId + ")의 좋아요가 완료되었습니다.\n좋아요 개수: " + countLikes(
+                entityId);
         } else {
-            validateLikeableEntityOwnership(likeableEntityId, entityType, user);
-            likeRepository.delete(optionalLike.get());
-            return false;
+            getLikeRepository().delete(optionalLike.get());
+            return getEntityName() + "(Id: " + entityId + ")의 좋아요가 취소되었습니다.\n좋아요 개수: " + countLikes(
+                entityId);
         }
     }
 
     /**
-     * 특정 엔티티에 대한 좋아요 개수를 반환합니다.
+     * 사용자가 좋아요 한 엔티티 목록을 조회합니다.
      *
-     * @param likeableEntityId   좋아요를 할 엔티티의 ID
-     * @param likeableEntityType 좋아요를 할 엔티티의 타입 (POST 또는 COMMENT)
-     * @return 좋아요 개수
+     * @param user     현재 인증된 사용자 정보
+     * @param pageable 페이지 정보
+     * @return 좋아요 한 엔티티 목록
      */
-    public int likeCount(Long likeableEntityId, String likeableEntityType) {
-        return likeRepository.findAllByLikeableEntityIdAndLikeableEntityType(likeableEntityId,
-            LikeableEntityType.getByType(likeableEntityType)).size();
+    @Transactional(readOnly = true)
+    public Page<D> getLikedEntities(User user, Pageable pageable) {
+        return getLikesByUser(user, pageable).map(this::convertToDto);
     }
 
-    /**
-     * 특정 엔티티의 소유권을 검증합니다. 사용자가 자신의 엔티티에 좋아요를 할 수 없도록 합니다.
-     *
-     * @param likeableEntityId   좋아요를 할 엔티티의 ID
-     * @param likeableEntityType 좋아요를 할 엔티티의 타입 (POST 또는 COMMENT)
-     * @param user               현재 인증된 사용자
-     */
-    private void validateLikeableEntityOwnership(
-        Long likeableEntityId,
-        LikeableEntityType likeableEntityType,
-        User user) {
+    protected abstract Class<T> getEntityClass();
 
-        if (likeableEntityType == LikeableEntityType.POST) {
-            Post post = postRepository.findById(likeableEntityId).orElseThrow(
-                () -> new EntityNotFoundException(NotFoundEntity.POST_NOT_FOUND));
-            validateOwnership(post.getUser().getId(), user.getId());
-        } else if (likeableEntityType == LikeableEntityType.COMMENT) {
-            Comment comment = commentRepository.findById(likeableEntityId).orElseThrow(
-                () -> new EntityNotFoundException(NotFoundEntity.COMMENT_NOT_FOUND));
-            validateOwnership(comment.getUser().getId(), user.getId());
-        } else {
-            throw new IllegalArgumentException("좋아요를 할 수 없는 엔티티 타입입니다.");
-        }
-    }
+    protected abstract Long getEntityOwnerId(T entity);
 
-    /**
-     * 특정 엔티티에 대해 락을 설정합니다.
-     *
-     * @param likeableEntityId   좋아요를 할 엔티티의 ID
-     * @param likeableEntityType 좋아요를 할 엔티티의 타입 (POST 또는 COMMENT)
-     */
-    private void lockEntity(Long likeableEntityId, LikeableEntityType likeableEntityType) {
-        if (likeableEntityType == LikeableEntityType.POST) {
-            entityManager.find(Post.class, likeableEntityId, LockModeType.PESSIMISTIC_WRITE);
-        } else if (likeableEntityType == LikeableEntityType.COMMENT) {
-            entityManager.find(Comment.class, likeableEntityId, LockModeType.PESSIMISTIC_WRITE);
-        }
-    }
+    protected abstract Optional<L> findLikeByEntityAndUser(T entity, User user);
 
-    /**
-     * 소유권을 검증합니다. 사용자가 자신의 엔티티에 대해 작업을 수행할 수 없도록 합니다.
-     *
-     * @param entityOwnerId 엔티티 소유자의 ID
-     * @param userId        현재 인증된 사용자 ID
-     */
+    protected abstract L createLike(User user, T entity);
+
+    protected abstract String getEntityName();
+
+    protected abstract JpaRepository<L, Long> getLikeRepository();
+
+    protected abstract long countLikes(Long entityId);
+
+    protected abstract Page<L> getLikesByUser(User user, Pageable pageable);
+
+    protected abstract D convertToDto(L like);
+
     private void validateOwnership(Long entityOwnerId, Long userId) {
         if (entityOwnerId.equals(userId)) {
             throw new UserActionNotAllowedException(UserActionError.SELF_ACTION_NOT_ALLOWED);
         }
     }
 }
-
