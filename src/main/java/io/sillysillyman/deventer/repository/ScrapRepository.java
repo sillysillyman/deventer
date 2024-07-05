918ebbbc8e133c57f6a9bd25801d522a72bedcdc
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-05 15:29:08 +0900

    feat: ScrapRepository에 페이지네이션 및 정렬 기능 추가
    - 사용자의 스크랩을 게시물 생성일자 기준으로 조회하는 기능 추가
    - 사용자의 스크랩을 게시물 작성자 닉네임 기준으로 조회하는 기능 추가
    - 기존의 findAllByUser 메서드를 페이지네이션 및 정렬이 가능한 메서드로 변경

--- src/main/java/io/sillysillyman/deventer/repository/ScrapRepository.java
+++ src/main/java/io/sillysillyman/deventer/repository/ScrapRepository.java
@@ -3,13 +3,16 @@ package io.sillysillyman.deventer.repository;
 import io.sillysillyman.deventer.entity.Post;
 import io.sillysillyman.deventer.entity.Scrap;
 import io.sillysillyman.deventer.entity.User;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 
     Optional<Scrap> findByUserAndPost(User user, Post post);
 
-    List<Scrap> findAllByUser(User user);
+    Page<Scrap> findAllByUserOrderByPostCreatedAtDesc(User user, Pageable pageable);
+
+    Page<Scrap> findAllByUserOrderByPostUserNicknameAsc(User user, Pageable pageable);
 }
